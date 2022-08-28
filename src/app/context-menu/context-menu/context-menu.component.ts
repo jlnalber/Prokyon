ee59446f762252716a7a2011eb67277913d7ad40
Julian Alber <alber.julian@outlook.com>
2022-08-28 12:11:44 +0200

    fix context menu #2

--- src/app/context-menu/context-menu/context-menu.component.ts
+++ src/app/context-menu/context-menu/context-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {ContextMenu, ContextMenuElement} from "../context-menu.directive";
 import {Point} from "../../global/interfaces/point";
 
@@ -7,19 +7,55 @@ import {Point} from "../../global/interfaces/point";
   templateUrl: './context-menu.component.html',
   styleUrls: ['./context-menu.component.css']
 })
-export class ContextMenuComponent implements OnInit {
+export class ContextMenuComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('wrapper') wrapper!: ElementRef;
+  private wrapperElement?: HTMLDivElement;
 
   public contextMenu: ContextMenu = {
     elements: []
   }
-  public position: Point = {
+
+  private _position: Point = {
     x: 0,
     y: 0
+  }
+  public get position(): Point {
+    // if the context menu is on the side of the page, prevent a "overflow" and position it back
+    if (this.wrapperElement) {
+      const viewportSize = {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
+      const rectContextMenu = this.wrapperElement.getBoundingClientRect();
+      return {
+        x: Math.min(this._position.x, viewportSize.width - rectContextMenu.width),
+        y: Math.min(this._position.y, viewportSize.height - rectContextMenu.height)
+      }
+    }
+
+    return this._position;
+  }
+  public set position(value: Point) {
+    this._position = value;
   }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.wrapperElement = this.wrapper.nativeElement as HTMLDivElement;
+    this.setPosition();
+  }
+
+  private setPosition(): void {
+    if (this.wrapperElement) {
+      const position = this.position;
+      this.wrapperElement.style.left = `${position.x}px`;
+      this.wrapperElement.style.top = `${position.y}px`;
+    }
   }
 
   click(event: Event, element: ContextMenuElement) {
