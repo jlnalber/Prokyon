1500270c1af843662f101888e55685d4a4d0b8f0
jlnalber <alber.julian@outlook.com>
2024-07-20 09:07:02 +0200

    ready for update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare const MathJax: any;
+
+console.log(MathJax)
+
 if (environment.production) {
   enableProdMode();
 }
